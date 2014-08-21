2dee798515b80f02fc21f9da7cf8d6193797c558
VampireTeeth <stevenl@ULT-STEVENL-L.dataprocessors.com.au>
2014-08-21 18:04:07 +1000

    typeclasses.hs updates

--- typeclasses.hs
+++ typeclasses.hs
@@ -1,12 +1,39 @@
-data Point = Point Float Float deriving (Show)
-data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+data Point = Point {
+             x :: Float,
+             y :: Float
+           } deriving (Show)
+
+data Shape = Circle {
+             point :: Point,
+             radius :: Float
+           } | Rectangle {
+             bottomLeft :: Point,
+             topRight :: Point
+           } deriving (Show)
 
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r * 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) dx dy = Circle (Point (x + dx) (y + dy)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) dx dy = Rectangle (Point (x1+dx) (y1+dy)) (Point (x2+dx) (y2+dy))
+
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRectangle :: Float -> Float -> Shape
+baseRectangle width height = Rectangle (Point 0 0) (Point width height)
+
+data Person = Person{ 
+  firstName :: String,
+  lastName :: String,
+  age :: Int,
+  height :: Float,
+  phoneNumber :: String,
+  flavor :: String
+} deriving (Show)
+
+
 
