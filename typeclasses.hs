03374e4d1a4683ff97ecc73facb1e254387edb97
VampireTeeth <vampire001@gmail.com>
2014-08-21 23:48:40 +1000

    typeclasses.hs updates

--- typeclasses.hs
+++ typeclasses.hs
@@ -1,3 +1,5 @@
+import qualified Data.Map as Map
+
 data Point = Point {
              x :: Float,
              y :: Float
@@ -40,7 +42,7 @@ data Car = Car {
            make :: String,
            model :: String,
            year :: Int
-         } deriving (Show)
+         } deriving (Show, Eq, Read)
 
 tellCar :: Car -> String
 tellCar (Car make model year) = "This " ++ make ++ " " ++ model ++ " was made in " ++ (show year)
@@ -56,3 +58,36 @@ vmult :: (Num t) => Vector t -> Vector t -> Vector t
 
 vscalarmulti :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `vscalarmulti` (Vector l m n) = i * l + j * m + k * n
+
+
+data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+left :: Prelude.Either a b -> Prelude.Maybe a
+left (Prelude.Left a) = Prelude.Just a
+left (Prelude.Right b) = Prelude.Nothing
+
+right :: Prelude.Either a b -> Prelude.Maybe b
+right (Prelude.Left a) = Prelude.Nothing
+right (Prelude.Right b) = Prelude.Just b
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Prelude.Either String Code
+lockerLookup lockerNumber map = 
+    case Map.lookup lockerNumber map of 
+        Prelude.Nothing -> Prelude.Left $ "Number " ++ show lockerNumber ++ " does not exist!"
+        Prelude.Just (state, code) -> if state == Taken
+                                   then Prelude.Left $ "Locker " ++ show lockerNumber ++ " is alread taken!"
+                                   else Prelude.Right code
+
+lockers :: LockerMap
+lockers = Map.fromList [
+     (100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]
