637a83382a36adf7b5b32d23f23ff6194f12fbac
VampireTeeth <stevenl@ULT-STEVENL-L.dataprocessors.com.au>
2014-08-25 17:14:59 +1000

    getContents.hs

---
+++ capslocker.hs
@@ -0,0 +1,12 @@
+import Control.Monad
+import Data.Char
+
+-- main = forever $ do
+--   putStrLn "Get me some text"
+--   l <- getLine
+--   putStrLn $ map toUpper l
+
+
+main = do
+    contents <- getContents
+    putStr $ map toUpper contents
